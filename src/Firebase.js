2b7275dda0a8a289b21f48333824d2c685414dea
Freddy419 <71938087+Ali-Akil-0@users.noreply.github.com>
2022-02-21 12:34:03 +0100

    Version 0.0.1

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDARu74uQ_vPUzzP0LKc_geqXmAGtiZoWs",
+    authDomain: "a2app-1d94a.firebaseapp.com",
+    projectId: "a2app-1d94a",
+    storageBucket: "a2app-1d94a.appspot.com",
+    messagingSenderId: "384485993120",
+    appId: "1:384485993120:web:63ac1cb547d7608685b7c5"
+  };
